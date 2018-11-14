ff508a52a8ecbae0085ce386ccb89dd6287c0a27
cgti <vsantos.ifto@gmail.com>
2018-11-13 21:43:16 -0300

    Segundo Commit

--- Site/core/views.py
+++ Site/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from Site.core.models import Funcionario
 
 # Create your views here.
 def Home (request):
-    pessoas = {'pessoa':('Ricardo','rua 01','9999-9999'), 'carro':('Ford', '1.6', 'verde')}
-    return render(request, 'index.html', pessoas)
+    funcionario = Funcionario.objects.all()
+
+    return render(request, 'index.html', {'funcionario': funcionario, 'teste': '122334'})
